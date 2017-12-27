5fca698c856f867b4049584ceeb15987dfa6ced5
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-12-27 10:00:56 +0530

    Added automatic wifi connection to boot sequence

---
+++ boot.py
@@ -0,0 +1,19 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+
+#import esp
+
+#esp.osdebug(None)
+
+import gc
+
+#import webrepl
+
+#webrepl.start()
+
+gc.collect()
+
+import wifi
+
+wifi.connect()
+
+
